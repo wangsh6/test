8be1e09db2bebd3c75215b7a137f85bd41bd6c80
wangsh6 <1135514730@qq.com>
2022-12-24 10:38:28 +0800

    提交文件

--- st_js.py
+++ st_js.py
@@ -3,7 +3,9 @@
 通过前端改写，设置 颜色、字体、等细节
 """
 
-import torch
+import jieba
 import streamlit as st
 
-st.write( str(torch.version) ) 
+corpus=['工信处女干事每月经过下属科室都要亲口交代24口交换机等技术性器件的安装工作。'] 
+tmp1=[[word for word in jieba.lcut(doc,cut_all=False)] for doc in corpus]
+st.write( tmp1 ) 
